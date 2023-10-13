403145c9baf6e804991f4725c56e5efe16bcedf5
Michael Testut <michael.testut@yahoo.com>
2023-10-12 22:07:29 -0700

    start home page styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,54 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
 
 export default function Home() {
     const navigate = useNavigate();
     return (
-        <div>
-            <h1 className="text-center">31 Nights of Horror</h1>
-            <h3> How Scary Do You Want?</h3>
-            <button onClick={() => navigate('/notscary')}>Not So Scary!</button>
-            <button onClick={() => navigate('/scary')}>Scary!</button>
-            <button onClick={() => navigate('/terrifying')}>Terrifying!</button>
+        <Container>
+            <div className='content'>
+                <h1 className="text-center">31 Nights of Horror</h1>
+                <h3> How Scary Can You Handle?</h3>
+                <div className='buttons'>
+                    <button onClick={() => navigate('/notscary')}>Not So Scary!</button>
+                    <button onClick={() => navigate('/scary')}>Scary!</button>
+                    <button onClick={() => navigate('/terrifying')}>Terrifying!</button>
+                </div>
 
-        </div>
+            </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    padding-top: 250px;
+    margin: 0px;
+    background-color:black;
+    background-size:100%;
+    margin:0;
+    .content{
+        display:flex;
+        flex-direction: column;
+        color:orange;
+        margin: 0;
+        text-align: center;
+        justify-content:center;
+        align-items:center;
+    }
+
+    .buttons{
+        width: 25vw;
+        display:flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+    button{
+        color: orange;
+        background-color:white;
+        border: none;
+
+    }
+
+`
