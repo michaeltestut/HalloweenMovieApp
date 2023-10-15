c23124fa7fab9d52e25c9f5607d84735c4bb355a
Michael Testut <michael.testut@yahoo.com>
2023-10-14 22:03:22 -0700

    update backend to typescript

---
+++ server/models/person.model.ts
@@ -0,0 +1,29 @@
+const mongoose2 = require ('mongoose')
+
+const PersonSchema = new mongoose2.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First Name is required'],
+        minlength: [2, 'First Name must be at least 2 characters']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last Name is required'],
+        minlength: [2, 'Last Name must be at least 2 characters']
+    },
+    birthdate: {
+        type: Date,
+        required: [true, 'Birthday is required']
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters long"],
+        maxlength: [12, "Password must be no longer than 12 characters"]
+    }
+}, { timestamps: true });
+module.exports = mongoose2.model('Person', PersonSchema);
